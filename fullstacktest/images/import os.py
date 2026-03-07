4aa5ac0edfa668f2245369fb4e5f4ba4d6578d65
gensokyoz1 <gensokyochen29@gmail.com>
2026-03-07 14:41:20 +0100

    initial website version

---
+++ fullstacktest/images/import os.py
@@ -0,0 +1,12 @@
+import os
+import time 
+
+print ("Mars Operating system") 
+
+time.sleep(2)
+
+choice = input ("Choose which program you'd like to monitor") 
+
+
+if choice == "A": 
+    print ("Hello")
